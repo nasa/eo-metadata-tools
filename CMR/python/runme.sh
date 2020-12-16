268473b9b6ec6a1c2fa0fd89d9faadb51639bbf3
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-12-16 11:46:35 -0500

    CMR-6818 - Change 2 - Now with GitHub instructions (#14)
    * adding basic files for creating a package
    * removing test from wheel and cleaning up the run script
    * updating the readme with now functionality
    * updating the demo files, removing step to manual load library and instead allow pip to handle things
    * updating documentation to show how to install from github

--- CMR/python/runme.sh
+++ CMR/python/runme.sh
@@ -7,10 +7,10 @@
 # Print out a usage manual
 help()
 {
-    echo 'Run all the code life cycle steps: lint, test, package, install, uninstall, and clean'
+    echo 'Run all the code stages: lint, test, package, install, uninstall, and clean'
     echo
     echo 'Usage:'
-    echo '    ./runme.sh -[chlpt] -[i | u]'
+    echo '    ./runme.sh -[cfhlpt] -[i | u]'
     echo '    ./runme.sh'
     echo '        same as ./runme.sh -l -t      #list, test'
     echo '    ./runme.sh -l -t -p -i            #lint, test, package, install'
@@ -20,7 +20,8 @@ help()
     printf "${format}" Flag Name Description
     printf "${format}" ---- ---------- ------------------------------
     printf "${format}" '-c' 'clean' 'Clean up all generated files and directories'
-    printf "${format}" '-h' 'help' 'Print out this help message'
+    printf "${format}" '-f' 'find' 'Find the package in pip3'
+    printf "${format}" '-h' 'help' 'Print out this help message and then exit'
     printf "${format}" '-i' 'install' 'Install latest wheel file'
     printf "${format}" '-u' 'uninstall' 'Uninstall the wheel file'
     printf "${format}" '-l' 'lint' 'Print out this help message'
