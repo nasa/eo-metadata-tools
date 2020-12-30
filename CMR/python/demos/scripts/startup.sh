4ef2c4d9867dc50fac4e6ca3192e6b0ac8d58ccc
Thomas Cherry <thomas.a.cherry@nasa.gov>
2020-12-30 16:46:34 -0500

    CMR-6950 - file added by mistake

--- CMR/python/demos/scripts/startup.sh
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.expanduser('~/src/project/eo-metadata-tools/CMR/python'))
-
-import cmr as CMR
-import cmr.search.collection as coll
-import cmr.search.granule as gran
-
-print ('welcome')
