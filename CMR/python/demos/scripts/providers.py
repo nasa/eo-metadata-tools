09c4d9a0bd8e120f6233212fabb16b4f525fcbc1
Thomas Cherry <thomas.a.cherry@nasa.gov>
2023-01-26 09:16:14 -0500

    CMR-8895 - adding a script used in development as it may help document the provider API

---
+++ CMR/python/demos/scripts/providers.py
@@ -0,0 +1,27 @@
+"""
+A very short demo of how to use the provider API
+"""
+
+import cmr.search.providers as p
+
+
+print(p.search_by_id(None))
+
+print ("*"*80)
+print(p.search_by_id(''))
+
+print ("*"*80)
+print(p.search_by_id('.*GHRC.*'))
+
+print ("*"*80)
+print(p.search_by_id('*GHRC*'))
+
+print ("*"*80)
+print(p.search_by_id('.*GHRC.*', {'env':'uat'}))
+
+print ("*"*80)
+result = p.search_by_id('.*GHRC.*', {'env':'uat'})
+if 'errors' in result:
+    print('found errors')
+else:
+    print (len(result))
